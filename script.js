48f0ab1b88022363f655899dd7be9b9b13f34bc7
LEXI-LAPTOP\lexis <lexisario5503@gmail.com>
2026-04-04 16:04:50 -0700

    final portal page development

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 
     const button = document.querySelector('#switch');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
     const footer = document.querySelector('footer');
     const sections = document.querySelectorAll('section')
     let mode = 'creation';
@@ -11,16 +10,20 @@
     button.addEventListener('click', function() {
         if (mode === 'creation') {
             body.className = 'switch';
+            body.style.backgroundImage = "url('images/rest.gif')";
             footer.className = 'switch';
             button.className = 'switch';
+            button.innerHTML = "rest.";
             for (const section of sections) {
                 section.className = 'switch';
             }
             mode = 'rest';
         } else {
             body.removeAttribute('class');
+            body.style.backgroundImage = "url('images/creation.gif')";
             footer.removeAttribute('class');
             button.removeAttribute('class');
+            button.innerHTML = "CREATION.";
             for (const section of sections) {
                 section.removeAttribute('class');
             }
